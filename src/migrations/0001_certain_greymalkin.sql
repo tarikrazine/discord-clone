da51ab7edc59b9ebd6d0539790ffced76e2d4a97
tarik razine <tarikrazine.om@gmail.com>
2023-09-17 21:32:12 +0100

    feat: add migrations

---
+++ src/migrations/0001_certain_greymalkin.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "member" ALTER COLUMN "profile_id" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "member" ALTER COLUMN "server_id" SET DATA TYPE text;--> statement-breakpoint
+ALTER TABLE "server" ALTER COLUMN "profile_id" SET DATA TYPE text;
