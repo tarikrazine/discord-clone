f72dd5e73bdefdbf0eed4badd22c0cb02a856c53
tarik razine <tarikrazine.com@gmail.com>
2023-10-13 00:05:27 +0100

    feat: change length

--- src/components/modals/membersModal.tsx
+++ src/components/modals/membersModal.tsx
@@ -65,10 +65,10 @@ function MembersModal() {
       const url = qs.stringifyUrl({
         url: `/api/members/${memberId}`,
         query: {
-          serverId: data.server?.id
+          serverId: data.server?.id,
         },
       });
-      
+
       const response = await axios.delete(url);
 
       router.refresh();
@@ -87,10 +87,10 @@ function MembersModal() {
       const url = qs.stringifyUrl({
         url: `/api/members/${memberId}`,
         query: {
-          serverId: data.server?.id
+          serverId: data.server?.id,
         },
       });
-      
+
       const response = await axios.patch(url, { role });
 
       router.refresh();
@@ -107,31 +107,26 @@ function MembersModal() {
       <DialogContent className="bg-white text-black overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Manage members
-            <DialogDescription className="text-center text-zinc-500">
-              {data.server?.members?.length} members
-            </DialogDescription>
+            Manage Members
           </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {length(data.server?.members)} Members
+          </DialogDescription>
         </DialogHeader>
         <ScrollArea className="mt-8 max-h-[420px] pr-6">
-          {data.server?.members.map((member) => {
-            const { profile } = member as any;
-
-            return (
-              <div key={member.id} className="flex items-center gap-x-2 mb-6">
-                <UserAvatar
-                  src={profile.imageUrl || undefined}
-                  name={profile.name!}
-                />
-                <div className="flex flex-col gap-y-1">
-                  <div className="text-xs font-semibold flex items-center gap-x-1">
-                    {profile.name} {roleIconMap[member.role!]}
-                  </div>
-                  <p className="text-xs text-zinc-500">{profile.email}</p>
+          {data.server?.members?.map((member) => (
+            <div key={member.id} className="flex items-center gap-x-2 mb-6">
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div className="text-xs font-semibold flex items-center gap-x-1">
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
                 </div>
-                <div className="ml-auto">
-                  {data.server?.profileId !== member.profileId &&
-                  loadingId !== member.id ? (
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
+              </div>
+              {data.server.profileId !== member.profileId &&
+                loadingId !== member.id && (
+                  <div className="ml-auto">
                     <DropdownMenu>
                       <DropdownMenuTrigger>
                         <MoreVertical className="h-4 w-4 text-zinc-500" />
@@ -139,28 +134,30 @@ function MembersModal() {
                       <DropdownMenuContent side="left">
                         <DropdownMenuSub>
                           <DropdownMenuSubTrigger className="flex items-center">
-                            <ShieldQuestion className="h-4 w-4 mr-2" />
+                            <ShieldQuestion className="w-4 h-4 mr-2" />
                             <span>Role</span>
                           </DropdownMenuSubTrigger>
                           <DropdownMenuPortal>
                             <DropdownMenuSubContent>
                               <DropdownMenuItem
-                                onClick={() => onRoleChange(member.id, "MODERATOR")}
-                              >
-                                <ShieldCheck className="h-4 w-4 mr-2" />
-                                Moderator
-                                {member.role === "MODERATOR" ? (
-                                  <Check className="h-4 w-4 ml-auto" />
-                                ) : null}
-                              </DropdownMenuItem>
-                              <DropdownMenuItem
                                 onClick={() => onRoleChange(member.id, "GUEST")}
                               >
                                 <Shield className="h-4 w-4 mr-2" />
                                 Guest
-                                {member.role === "GUEST" ? (
+                                {member.role === "GUEST" && (
                                   <Check className="h-4 w-4 ml-auto" />
-                                ) : null}
+                                )}
+                              </DropdownMenuItem>
+                              <DropdownMenuItem
+                                onClick={() =>
+                                  onRoleChange(member.id, "MODERATOR")
+                                }
+                              >
+                                <ShieldCheck className="h-4 w-4 mr-2" />
+                                Moderator
+                                {member.role === "MODERATOR" && (
+                                  <Check className="h-4 w-4 ml-auto" />
+                                )}
                               </DropdownMenuItem>
                             </DropdownMenuSubContent>
                           </DropdownMenuPortal>
@@ -172,14 +169,13 @@ function MembersModal() {
                         </DropdownMenuItem>
                       </DropdownMenuContent>
                     </DropdownMenu>
-                  ) : null}
-                  {loadingId === member.id ? (
-                    <Loader2 className="animate-spin text-zinc-500 ml-auto w-4 h-4" />
-                  ) : null}
-                </div>
-              </div>
-            );
-          })}
+                  </div>
+                )}
+              {loadingId === member.id && (
+                <Loader2 className="animate-spin text-zinc-500 ml-auto w-4 h-4" />
+              )}
+            </div>
+          ))}
         </ScrollArea>
       </DialogContent>
     </Dialog>
