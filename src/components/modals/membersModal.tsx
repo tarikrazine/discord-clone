29f2b0fe9ffb85e2be0bc433edb066b917ce77aa
tarik razine <tarikrazine.com@gmail.com>
2023-10-13 20:15:53 +0100

    feat: retest

--- src/components/modals/membersModal.tsx
+++ src/components/modals/membersModal.tsx
@@ -110,7 +110,7 @@ function MembersModal() {
             Manage Members
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            {data.server?.members?.length > 0 ? <>{data.server?.members?.length} Members</>  : null}
+            {data.server?.members ? <>{data.server?.members?.length} Members</>  : null}
           </DialogDescription>
         </DialogHeader>
         <ScrollArea className="mt-8 max-h-[420px] pr-6">
